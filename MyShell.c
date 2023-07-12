271d115af8ce6eda2ad9b0e09312879720967e7f
Igor <s02200334@gse.cs.msu.ru>
2023-07-12 22:12:55 +0300

    Done 3

--- MyShell.c
+++ MyShell.c
@@ -4,12 +4,61 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <fcntl.h>
 
 int contrlc(char c){
 	if ((c=='&')||(c=='|')||(c==';')||(c=='>')||(c=='<')||(c=='(')||(c==')'))
 		return 0;
 	else 
 		return 1;
+}
+
+int conv(char** str, int f0, int f1){
+	int fd[2], p;
+	if(!contrlc(str[0][0])){
+		fprintf(stderr, "ошибка");
+		return 1;
+	};
+	int i=0;
+	for(;str[i]!=NULL;){
+		int j=0, knc=1, knc2;
+		for(;((str[i]!=NULL)&&(str[i][0]!='|'));i++,j++);
+		knc2=i-j;
+		if(str[i]){
+			free(str[i]);
+			str[i] = NULL;
+			i++;
+		}else{
+			knc=0;
+		}
+		pipe(fd);
+		p=fork();
+		if (p==-1){
+			perror("ошибка");
+			return 1;
+		}else if (p==0){
+				if(f0!=-1){
+					dup2(f0,0);
+				}
+				close(fd[0]);
+				close(f0);
+				if(knc){
+					dup2(fd[1], 1);
+				}else if (f1!=-1)
+						dup2(f1, 1);
+				close(fd[1]);
+				execvp(str[knc2], &(str[knc2]));
+				perror("ошибка");
+				exit(1);
+			}else{
+				close(f0);
+				f0=fd[0];
+				close(fd[1]);
+			}
+	}
+	close(f0);
+	while(wait(NULL)!=-1);
+	return 0;
 }
 int main(int argc, char *argv[]){
 	FILE* inp=stdin;
@@ -78,22 +127,44 @@ int main(int argc, char *argv[]){
 				
 			}
 		}
+		int f0=-1, f1=-1;
 		str[nw-1]=NULL;
 		if(str[0]){
 			if(strcmp(str[0], "cd")){
-				i=0;
-				int pid;
-				if((pid=fork())==0){
-					execvp(str[0], str);
-					perror("ошибка");
-					return 1;
-				};
-				int status;
-				wait(&status);
-				if(pid==-1){
-					perror("ошибка");
-					return 1;
+				int i=0;
+				if (!(contrlc(str[i][0]))){
+					if((str[i][0]=='>')||(str[i][0]=='<')){
+						if (str[i][0]=='<'){
+							if ((f0=open(str[i+1], O_RDONLY, 0777))==-1)
+								perror("Ошибка");
+						}else if(strcmp(str[i], ">>"))
+								f1=open(str[i+1], O_CREAT | O_WRONLY | O_TRUNC, 0777);
+							else
+								f1=open(str[i+1], O_CREAT | O_WRONLY | O_APPEND, 0777);
+						i=i+2;
+					}else 
+						fprintf(stderr, "Ошибка");
 				}
+				if (!(contrlc(str[i][0]))){
+					if((str[i][0]=='>')||(str[i][0]=='<')){
+						if (str[i][0]=='<'){
+							if (f0==-1){
+								if ((f0=open(str[i+1], O_RDONLY, 0777))==-1)
+									perror("Ошибка");
+							}else 
+								fprintf(stderr, "Ошибка");
+						}else if (f1==-1){
+								if(strcmp(str[i], ">>"))
+									f1=open(str[i+1], O_CREAT | O_WRONLY | O_TRUNC, 0777);
+								else
+									f1=open(str[i+1], O_CREAT | O_WRONLY | O_APPEND, 0777);
+							}else 
+								fprintf(stderr, "Ошибка");
+							i=i+2;
+					}else 
+						fprintf(stderr, "Ошибка");
+				}
+				conv(&(str[i]), f0, f1);
 			}else{
 				if (str[1]==NULL)
 					chdir(home);
@@ -103,7 +174,8 @@ int main(int argc, char *argv[]){
 			}
 		}
 		for (i=0; i<nw-1; i++)
-			free(str[i]);
+			if (str[i])
+				free(str[i]);
 		free(str);
 		printf("$");
 	}
