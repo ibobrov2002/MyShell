2f2bba411bebfe8cf7158521a5a8db9b91ac7121
Igor <s02200334@gse.cs.msu.ru>
2023-07-12 22:13:32 +0300

    Done 4

--- MyShell.c
+++ MyShell.c
@@ -19,7 +19,7 @@ int conv(char** str, int f0, int f1){
 		fprintf(stderr, "ошибка");
 		return 1;
 	};
-	int i=0;
+	int i=0, pr=0, status;
 	for(;str[i]!=NULL;){
 		int j=0, knc=1, knc2;
 		for(;((str[i]!=NULL)&&(str[i][0]!='|'));i++,j++);
@@ -32,6 +32,7 @@ int conv(char** str, int f0, int f1){
 			knc=0;
 		}
 		pipe(fd);
+		pr++;
 		p=fork();
 		if (p==-1){
 			perror("ошибка");
@@ -57,7 +58,7 @@ int conv(char** str, int f0, int f1){
 			}
 	}
 	close(f0);
-	while(wait(NULL)!=-1);
+	waitpid(p, &status, 0);
 	return 0;
 }
 int main(int argc, char *argv[]){
@@ -69,7 +70,8 @@ int main(int argc, char *argv[]){
 			return 1;
 		}
 	}
-	int c, lw=10, i;
+	int c, lw=10, i, lp=2, status;
+	int* pid=(int*)malloc(sizeof(int)*10);
 	printf("$");
 	while ((c=fgetc(inp))!=EOF){
 		char** str;
@@ -143,7 +145,7 @@ int main(int argc, char *argv[]){
 								f1=open(str[i+1], O_CREAT | O_WRONLY | O_APPEND, 0777);
 						i=i+2;
 					}else 
-						fprintf(stderr, "Ошибка");
+						fprintf(stderr, "Ошибка1");
 				}
 				if (!(contrlc(str[i][0]))){
 					if((str[i][0]=='>')||(str[i][0]=='<')){
@@ -162,9 +164,37 @@ int main(int argc, char *argv[]){
 								fprintf(stderr, "Ошибка");
 							i=i+2;
 					}else 
-						fprintf(stderr, "Ошибка");
+						fprintf(stderr, "Ошибка2");
+				};
+				int j=0;
+				for(;((str[j]!=NULL)&&(contrlc(str[j][0]))); j++);
+				if((str[j]!=NULL)&&(str[j][0]=='&')){
+					int p;
+					free(str[j]);
+					str[j]=NULL;
+					p=fork();
+					if (p==-1){
+						perror("ошибка");
+						return 1;
+					}else if (p==0){
+						int fnull;
+						if ((fnull=open("/dev/null", O_RDWR))==-1)
+							perror("Ошибка");
+						dup2(fnull, 0);
+						dup2(fnull, 1);
+						signal(SIGINT, SIG_IGN);
+						execvp(str[0], str);
+						perror("ошибка");
+						exit(1);
+						}else{
+							if(!lp%10)
+								pid=(int*)realloc(pid, sizeof(int)*(lp+10));
+							pid[lp-2]=p;
+							lp++;
+						}
+				}else{
+					conv(&(str[i]), f0, f1);
 				}
-				conv(&(str[i]), f0, f1);
 			}else{
 				if (str[1]==NULL)
 					chdir(home);
@@ -173,12 +203,21 @@ int main(int argc, char *argv[]){
 						perror("ошибка");
 			}
 		}
+		int i;
+		for(i=0; i<lp-2; i++){
+			if (pid[i])
+				if (pid[i]==waitpid(pid[i], &status, WNOHANG)){
+					printf("%d %d\n", pid[i], status);
+					pid[i]=0;
+				}
+		};
 		for (i=0; i<nw-1; i++)
 			if (str[i])
 				free(str[i]);
 		free(str);
 		printf("$");
 	}
+	free(pid);
 	if (inp!=stdin)
 		fclose(inp);
 		
